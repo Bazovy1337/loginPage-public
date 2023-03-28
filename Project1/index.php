4886145b29a787fb41e82b8967a0058b2dc9a684
Bazovy1337 <125508489+Bazovy1337@users.noreply.github.com>
2023-03-28 22:27:37 +0200

    Add files via upload

---
+++ Project1/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="main.js" defer></script>
+    <title>Bazovy</title>
+</head>
+<body>
+    <div class="logo">
+        <h1 class="nav">Social Media</h1>
+        <img src="social.png" width="150px" class="nav">
+    </div>
+    <div class="loginW">
+        <div class="register">
+            <h2>Nie masz konta?</h2> <a href="index.php" class="rejestr">Zarejestruj się</a>
+        </div>
+        <div class="loginBox">
+            <div class="inputs">
+                <form>
+                    <p><b>Zaloguj się</b></p>
+                    <b>email</b><input type="email" class="email inputText" placeholder="Podaj email">
+                    <b>password</b><input type="password" class="password inputText" placeholder="Podaj hasło">
+                    <input type="submit" value="login" class="submit">
+                </form>
+            </div>
+                <div class="forgor">
+                    <a href="index.php" class="forgor">Nie pamiętasz hasła?</a>
+                </div>
+            </div>
+    </div>
+</body>
+</html>
